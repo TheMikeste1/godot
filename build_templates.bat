034cbdc2dbc602c30e24ea03e6747164f80d4620
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-11-26 22:32:40 -0700

    Separate templates from main build script

--- build_templates.bat
+++ build_templates.bat
@@ -2,6 +2,9 @@
 
 scons precision=double platform=windows production=yes module_mono_enabled=yes
 
+scons precision=double platform=windows target=template_debug   module_mono_enabled=yes
+scons precision=double platform=windows target=template_release module_mono_enabled=yes
+
 bin/godot.windows.editor.double.x86_64.mono --headless --generate-mono-glue modules/mono/glue
 
-./modules/mono/build_scripts/build_assemblies.py --precision=double --godot-output-dir=./bin --godot-platform=windows
+./modules/mono/build_scripts/build_assemblies.py --precision=double --godot-output-dir=./bin --godot-platform=windows 
